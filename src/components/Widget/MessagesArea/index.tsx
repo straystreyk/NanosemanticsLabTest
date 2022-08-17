820a63fe2043f633a9b35b4e1cd273ef50b4f791
tigran.tskanyan <tigran.tskanyan@spbtv.com>
2022-08-17 11:29:56 +0300

    reverse messeges in the area

--- src/components/Widget/MessagesArea/index.tsx
+++ src/components/Widget/MessagesArea/index.tsx
@@ -6,11 +6,14 @@ import { Message } from "./message";
 
 export const MessagesArea: React.FC = React.memo(() => {
   const { messages } = useMessages();
+  const reversed = React.useMemo(() => [...messages].reverse() ,[messages])
+
+
 
   return (
     <div className={classes.area}>
-      {messages && messages.length
-        ? messages.map((message) => {
+      {reversed && reversed.length
+        ? reversed.map((message) => {
             return <Message key={message.id} {...message} />;
           })
         : null}
